2cf81c67c7c1a695125aba49aa6935574bbd674f
jesus-espitia <espitiaguzmanjesusmanuel@gmail.com>
2024-11-09 22:52:59 +0100

    confirmacion jesus

--- vacaciones/app/app.py
+++ vacaciones/app/app.py
@@ -1,1 +1,5 @@
-import flask from flask
+import flask_login
+from flask import Flask, url_for, redirect, request, render_template, session, jsonify, flash
+from routes import miembros, login, inicio, logining, perfil, grupos, amigos_chat
+import mysql.connector
+import hashlib
