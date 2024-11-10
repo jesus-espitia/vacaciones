d2534bb567689877d2d125e57dba527125d63593
jesus_espitia66 <130522896+jesus-espitia@users.noreply.github.com>
2024-11-10 06:01:53 +0100

    Delete vacaciones/app directory

--- vacaciones/app/app.py
+++
@@ -1,5 +0,0 @@
-import flask_login
-from flask import Flask, url_for, redirect, request, render_template, session, jsonify, flash
-from routes import miembros, login, inicio, logining, perfil, grupos, amigos_chat
-import mysql.connector
-import hashlib
