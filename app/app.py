6c635644e88f73e04fb9dc44189d5421f5087c08
jesus-espitia <espitiaguzmanjesusmanuel@gmail.com>
2024-11-10 12:09:30 +0100

    confirmacion de actualizacion

--- app/app.py
+++ app/app.py
@@ -1,5 +1,21 @@
 import flask_login
 from flask import Flask, url_for, redirect, request, render_template, session, jsonify, flash
-from routes import miembros, login, inicio, logining, perfil, grupos, amigos_chat
+# from routes import Direciones_de_ruta
 import mysql.connector
 import hashlib
+
+app = Flask(__name__)
+
+app.secret_key = 'secret_key'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
